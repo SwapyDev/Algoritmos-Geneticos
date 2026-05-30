87e46469a3ecb710d08a4f7c980de849331661f1
SwapyDev <sabraham2342@gmail.com>
2026-05-30 14:56:44 -0600

    Helper functions

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import re
 
 def read_corpus(file) -> list:
     with open(file, "r", encoding="utf-8") as f:
-        content =  f.read()
-        return content.lower().split()
+        content = f.read()
+    tokens = content.lower().split()
+    clean = []
+    for word in tokens:
+        token = re.sub(r"[^a-záéíóúüñ]", "", word)
+        if token:
+            clean.append(token)
+    return clean
 
 #we need to count every time a bigram appears
 def build_bigram(text:list) -> dict:
@@ -28,6 +34,14 @@ def check_freq(bigram_list:dict, threshold:int) -> dict|string:
         return filtered
     return "Theres no bigrams with this frequency"
 
+#check desired word frequency
+def check_word(desired_word:str, text:list) -> int|string:
+    counter = 0
+    for word in text:
+        if desired_word == word:
+            counter += 1
+    return counter
+
 #create vocabulary
 def create_vocabulary(text) -> set:
     #set doesn't allow duplicates so its easiers
@@ -38,13 +52,18 @@ def create_vocabulary(text) -> set:
             words.add(token)
     return words
 
+#create unique words with index
 def create_unique_words(text) -> dict:
     dictionary = {word:index for index,word in enumerate(text)}
     return dictionary
+
+#dios: 15651
+#dijo: 23534,
 
 if __name__ == "__main__":
     list_of_words = read_corpus("biblia.txt")
     bigram_count = build_bigram(list_of_words)
     #print(check_freq(bigram_count, 500))
     vocab = create_vocabulary(list_of_words)
-    print(vocab)
+    #print(create_unique_words(vocab))
+    print(check_word("dios", list_of_words))
